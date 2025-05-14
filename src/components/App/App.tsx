9ce8d9edd2568e2446ef5ae5a0c682d8cc789f31
Marakas <igorpetriv.bsc@gmail.com>
2025-05-14 22:26:54 +0300

    finished without styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,8 @@ import Cafeinfo from "../CafeInfo/CafeInfo";
 import type { Votes } from "../types/votes";
 import { type VoteType } from "../types/votes";
 import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
 const votes: Votes = { good: 0, neutral: 0, bad: 0 };
 
@@ -19,11 +21,33 @@ export default function App() {
 
   const resetVote = (): void => setVote(votes);
 
+  const totalVotes: number = Object.values(vote).reduce(
+    (sum, value) => sum + value,
+    0
+  );
+
+  const positiveRate: number = totalVotes
+    ? Math.round((vote.good / totalVotes) * 100)
+    : 0;
+
   return (
     <>
       <div className={css.app}>
         <Cafeinfo />
-        <VoteOptions />
+        <VoteOptions
+          onVote={handleVote}
+          onReset={resetVote}
+          canReset={totalVotes > 0}
+        />
+        {totalVotes ? (
+          <VoteStats
+            votes={vote}
+            totalVotes={totalVotes}
+            positiveRate={positiveRate}
+          />
+        ) : (
+          <Notification />
+        )}
       </div>
     </>
   );
