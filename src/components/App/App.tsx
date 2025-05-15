0c1453529ba45deb8ffe09bbff3bfa1ae17a9ec5
Marakas <igorpetriv.bsc@gmail.com>
2025-05-15 21:14:40 +0300

    improved variable names

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,39 +1,39 @@
 import { useState } from "react";
 import css from "./App.module.css";
-import Cafeinfo from "../CafeInfo/CafeInfo";
 import type { Votes } from "../../types/votes";
 import { type VoteType } from "../../types/votes";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
+import CafeInfo from "../CafeInfo/CafeInfo";
 
-const votes: Votes = { good: 0, neutral: 0, bad: 0 };
+const initialVotes: Votes = { good: 0, neutral: 0, bad: 0 };
 
 export default function App() {
-  const [vote, setVote] = useState<Votes>(votes);
+  const [votes, setVote] = useState<Votes>(initialVotes);
 
   const handleVote = (type: VoteType): void => {
     setVote({
-      ...vote,
-      [type]: vote[type] + 1,
+      ...votes,
+      [type]: votes[type] + 1,
     });
   };
 
-  const resetVote = (): void => setVote(votes);
+  const resetVote = (): void => setVote(initialVotes);
 
-  const totalVotes: number = Object.values(vote).reduce(
+  const totalVotes: number = Object.values(votes).reduce(
     (sum, value) => sum + value,
     0
   );
 
   const positiveRate: number = totalVotes
-    ? Math.round((vote.good / totalVotes) * 100)
+    ? Math.round((votes.good / totalVotes) * 100)
     : 0;
 
   return (
     <>
       <div className={css.app}>
-        <Cafeinfo />
+        <CafeInfo />
         <VoteOptions
           onVote={handleVote}
           onReset={resetVote}
@@ -41,7 +41,7 @@ export default function App() {
         />
         {totalVotes ? (
           <VoteStats
-            votes={vote}
+            votes={votes}
             totalVotes={totalVotes}
             positiveRate={positiveRate}
           />
